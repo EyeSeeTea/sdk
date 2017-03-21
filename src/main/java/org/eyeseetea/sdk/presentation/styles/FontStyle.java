dcae54ee44efacb20882d0fbbc574e29b6e49f4d
Jorge Sánchez <xurxosf@gmail.com>
2017-03-21 09:47:37 +0100

    set title enum style as string key

--- src/main/java/org/eyeseetea/sdk/presentation/styles/FontStyle.java
+++ src/main/java/org/eyeseetea/sdk/presentation/styles/FontStyle.java
@@ -3,11 +3,11 @@ package org.eyeseetea.sdk.presentation.styles;
 import org.eyeseetea.sdk.R;
 
 public enum FontStyle {
-    XSmall(R.style.FontStyle_Small, "XSmall"),
-    Small(R.style.FontStyle_Small, "Small"),
-    Medium(R.style.FontStyle_Medium, "Medium"),
-    Large(R.style.FontStyle_Large, "Large"),
-    XLarge(R.style.FontStyle_Small, "XLarge");
+    XSmall(R.style.FontStyle_XSmall, "font_extra_small"),
+    Small(R.style.FontStyle_Small, "font_small"),
+    Medium(R.style.FontStyle_Medium, "font_medium"),
+    Large(R.style.FontStyle_Large, "font_large"),
+    XLarge(R.style.FontStyle_XLarge, "font_extra_large");
 
     private int resId;
     private String title;
