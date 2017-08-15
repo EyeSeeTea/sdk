49010286013c1db73afa1bec860c96a4e1522812
idelcano <ignaciodelcano@gmail.com>
2017-08-15 21:42:36 +0200

    fix downloader

--- src/main/java/org/eyeseetea/sdk/data/DownloadMediaTask.java
+++ src/main/java/org/eyeseetea/sdk/data/DownloadMediaTask.java
@@ -2,6 +2,7 @@ package org.eyeseetea.sdk.data;
 
 import android.os.AsyncTask;
 import android.util.Log;
+
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 
@@ -14,7 +15,7 @@ import java.util.List;
  * AsyncTask that downloads media files
  * Created by arrizabalaga on 28/05/16.
  */
-public class DownloadMediaTask extends AsyncTask<List<String>, Void, HashMap<String, String>> {
+public class DownloadMediaTask extends AsyncTask<Void, Void, HashMap<String, String>> {
 
     public interface Callback {
         void onError(Exception error);
@@ -37,7 +38,8 @@ public class DownloadMediaTask extends AsyncTask<List<String>, Void, HashMap<Str
     /**
      * Builds a task that requires service credentials
      */
-    public DownloadMediaTask(java.io.File filesDir, Drive googleService, List<String> uids, Callback callback) {
+    public DownloadMediaTask(java.io.File filesDir, Drive googleService, List<String> uids,
+            Callback callback) {
         mGoogleService = googleService;
         mUids = uids;
         mCallback = callback;
@@ -45,23 +47,22 @@ public class DownloadMediaTask extends AsyncTask<List<String>, Void, HashMap<Str
     }
 
     @Override
-    protected HashMap<String, String> doInBackground(List<String>... params) {
+    protected HashMap<String, String> doInBackground(Void... params) {
         Log.d(TAG, String.format("DownloadMediaTask starts"));
         HashMap<String, String> syncedFiles = new HashMap<>();
-        List<String> uidList = params[0];
         //Nothing to sync -> 0
-        if (uidList == null || uidList.isEmpty()) {
+        if (mUids == null || mUids.isEmpty()) {
             Log.d(TAG, String.format("DownloadMediaTask nothing to sync"));
             return null;
         }
 
-        for (String uid : uidList) {
+        for (String uid : mUids) {
             try {
                 String resourcePath = downloadFile(uid);
                 syncedFiles.put(uid, resourcePath);
                 Log.d(TAG, String.format("DownloadMediaTask file synced"));
             } catch (Exception e) {
-                if(e instanceof IllegalStateException){
+                if (e instanceof IllegalStateException) {
                     mCallback.removeIllegalFile(uid);
                 }
                 e.printStackTrace();
@@ -73,7 +74,7 @@ public class DownloadMediaTask extends AsyncTask<List<String>, Void, HashMap<Str
     }
 
     @Override
-    protected void onPostExecute(HashMap<String, String>  syncedFiles) {
+    protected void onPostExecute(HashMap<String, String> syncedFiles) {
         mCallback.onSuccess(syncedFiles);
     }
 
